02ca87593dc288bf7e014e47f978c4825bcc10a4
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-06 12:53:33 -0600

    added comments

--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import time
 physicsClient = p.connect(p.GUI)
 for i in range(1000): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
     p.stepSimulation()
-    time.sleep(0.005)
+    time.sleep(0.05) #time.sleep(0.005) is nice viewing time, not too slow
     print(i)
+    #to move the camera, control+click and drag with a mouse, or 2-fingered swipe on trackpad for zooming in/out
 p.disconnect()
