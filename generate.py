773fb8d79c751939c8b9ac4194bcced3e5e1e76e
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-06 13:40:34 -0600

    Add pyrosim

--- generate.py
+++ generate.py
@@ -1,1 +1,8 @@
 #generate.py
+####to run this, go into terminal: 'python .\generate.py' so that it generates the box.sdf file in the specified path
+#use pyrosim to generate a link, store it in a special file called world.sdf, and then simulate.py will read and simulate it 
+import pyrosim.pyrosim as pyrosim #import pyrosim
+pyrosim.Start_SDF("box.sdf") #tells pyrosim the name of the file where info about the world should be stored (in this case, it's a box)
+print("here")
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1]) #stores a box with initial position x=0, y=0, z=0.5 and length, width, and height = 1m, in box.sdf
+pyrosim.End() #tells pyrosim to close the sdf file
--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf") #tells pybullet to read in the world described in box.sdf
 for i in range(1000): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
     p.stepSimulation()
     time.sleep(0.05) #time.sleep(0.005) is nice viewing time, not too slow
