ad606a0ca9416fbf722dd1854f9fc1198ccb1312
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-06 12:43:50 -0600

    Added sleep and step in simulation

--- simulate.py
+++ simulate.py
@@ -1,4 +1,9 @@
 #trial of simulate.py
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+for i in range(1000): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
+    p.stepSimulation()
+    time.sleep(0.005)
+    print(i)
 p.disconnect()
