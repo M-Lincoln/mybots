74f330ae4e541281cee3ce5f66e5d04a7f530363
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-06 14:16:21 -0600

    add another link

--- generate.py
+++ generate.py
@@ -2,7 +2,14 @@
 ####to run this, go into terminal: 'python .\generate.py' so that it generates the box.sdf file in the specified path
 #use pyrosim to generate a link, store it in a special file called world.sdf, and then simulate.py will read and simulate it 
 import pyrosim.pyrosim as pyrosim #import pyrosim
-pyrosim.Start_SDF("box.sdf") #tells pyrosim the name of the file where info about the world should be stored (in this case, it's a box)
+pyrosim.Start_SDF("boxes.sdf") #tells pyrosim the name of the file where info about the world should be stored (in this case, it's a box)
 print("here")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1]) #stores a box with initial position x=0, y=0, z=0.5 and length, width, and height = 1m, in box.sdf
+length=1
+width=1
+height=1
+x=0
+y=0
+z=height/2 #makes it so that the initial position of the box is at ground-level
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[width,length,height]) #stores a box with initial position x, y, z and length, width, and height, in box.sdf
+pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[width,length,height]) #stores a box with initial position x, y, z and length, width, and height, in box.sdf
 pyrosim.End() #tells pyrosim to close the sdf file
