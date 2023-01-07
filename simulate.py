3a3d584cc0d5ed2f0c3b53b934f83ef63512786d
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-07 14:29:53 -0600

    1 Box at Origin

--- simulate.py
+++ simulate.py
@@ -9,7 +9,7 @@ planeID=p.loadURDF("plane.urdf") #add a floor to the environment
 p.loadSDF("boxes.sdf")
 for i in range(1000): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
     p.stepSimulation()
-    time.sleep(0.05) #time.sleep(0.005) is nice viewing time, not too slow
+    time.sleep(.005) #time.sleep(0.005) is nice viewing time, not too slow
     print(i) 
     #to move the camera, control+click and drag with a mouse, or 2-fingered swipe on trackpad for zooming in/out
 p.disconnect()
