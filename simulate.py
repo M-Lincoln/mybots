f33d2c9943745767b0ba46521226d7f4a095765d
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-15 17:43:52 -0600

    Revert "update the sensor values of the simulation and print the variable"
    This reverts commit 80bfbc33eadfffb7f96053c852f25c74da68d7ac.

--- simulate.py
+++ simulate.py
@@ -12,12 +12,13 @@ robotID=p.loadURDF("body.urdf") #add a torso to the environment
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotID) #pyrosim needs to set up for simulating sensors. robotID contains an integer, indicating which robot you want prepared for simulation
 backLegSensorValues = numpy.zeros(1000)
+print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
+exit()
 for i in range(1000): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
     p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg") #add a touch sensor to the back leg
-    print("backLegTouch sensor value = %d" %backLegSensorValues[i]) #print the sensor value of backLegTouch
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg") #add a touch sensor to the back leg
+    print("backLegTouch sensor value = %d" %backLegTouch) #print the sensor value of backLegTouch
     time.sleep(.05) #time.sleep(0.005) is nice viewing time, not too slow
     print(i) 
     #to move the camera, control+click and drag with a mouse, or 2-fingered swipe on trackpad for zooming in/out
 p.disconnect()
-print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
