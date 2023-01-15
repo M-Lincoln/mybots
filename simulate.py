e19e603634c30962cd4cb55bdb40b880a803ae2a
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-15 17:50:15 -0600

    install touch sensor to the back leg

--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 #trial of simulate.py
+import pyrosim.pyrosim as pyrosim #import pyrosim
 import pybullet as p
 import pybullet_data
 import time
@@ -8,8 +9,11 @@ p.setGravity(0,0,-9.8)#add gravity
 planeID=p.loadURDF("plane.urdf") #add a floor to the environment
 robotID=p.loadURDF("body.urdf") #add a torso to the environment
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotID) #pyrosim needs to set up for simulating sensors. robotID contains an integer, indicating which robot you want prepared for simulation
 for i in range(1000): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg") #add a touch sensor to the back leg
+    print("backLegTouch sensor value = %d" %backLegTouch) #print the sensor value of backLegTouch
     time.sleep(.05) #time.sleep(0.005) is nice viewing time, not too slow
     print(i) 
     #to move the camera, control+click and drag with a mouse, or 2-fingered swipe on trackpad for zooming in/out
