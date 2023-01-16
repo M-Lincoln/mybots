2fec53fb636a5e3b78583ecd2afc65ee53470005
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-16 11:52:43 -0600

    Prettifying the sensor data plot

--- analyze.py
+++ analyze.py
@@ -6,8 +6,8 @@ frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy',mmap_mode ='r'
 print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
 print("frontLegSensorValues = ",frontLegSensorValues) #printing array of frontLegSensorValues
 
-matplotlib.pyplot.plot(backLegSensorValues, label = "back leg sensor values") #plot the values of backLegSensorValues array
-matplotlib.pyplot.plot(frontLegSensorValues, label = "front leg sensor values") #plot the values of frontLegSensorValues array
+matplotlib.pyplot.plot(backLegSensorValues, linewidth = 3, label = "back leg sensor values") #plot the values of backLegSensorValues array
+matplotlib.pyplot.plot(frontLegSensorValues, linestyle = 'dashed', label = "front leg sensor values") #plot the values of frontLegSensorValues array
 
 matplotlib.pyplot.legend(loc="upper left")
 matplotlib.pyplot.show() #show the above plot
