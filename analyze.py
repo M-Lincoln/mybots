11c34c20d455c48e321767b2aaad61ce2f422f78
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-16 11:34:10 -0600

    Plot values of backLegSensorValues

--- analyze.py
+++ analyze.py
@@ -1,4 +1,7 @@
 #analyze.py
+import matplotlib.pyplot
 import numpy
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy',mmap_mode ='r', allow_pickle= False, fix_imports = False) #load data/backLegSensorValues.npy (for reading only = 'mmap_mode = r' into the vector backLegSensorValues.
 print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
+matplotlib.pyplot.plot(backLegSensorValues) #plot the values of backLegSensorValues array
+matplotlib.pyplot.show() #show the above plot
