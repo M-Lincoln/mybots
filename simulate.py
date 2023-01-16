57c983c4ffb488c7d30e9270fe0436f2605c1cc8
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-15 18:01:42 -0600

    Save Sensor values to computer

--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import pybullet as p
 import pybullet_data
 import time
 import numpy
+import os #need this to be able to save a variable in another directory/folder
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)#add gravity
@@ -16,8 +17,9 @@ for i in range(1000): #for loop going from 0-999, end with colon and make sure n
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg") #add a touch sensor to the back leg
     print("backLegTouch sensor value = %d" %backLegSensorValues[i]) #print the sensor value of backLegTouch
-    time.sleep(.05) #time.sleep(0.005) is nice viewing time, not too slow
+    time.sleep(.005) #time.sleep(0.005) is nice viewing time, not too slow
     print(i) 
     #to move the camera, control+click and drag with a mouse, or 2-fingered swipe on trackpad for zooming in/out
 p.disconnect()
 print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
+numpy.save(os.path.join('data','backLegSensorValues'),backLegSensorValues, allow_pickle=True, fix_imports=False) #save an array to a binary file in Numpy, .npy format, in a different folder called "data"
