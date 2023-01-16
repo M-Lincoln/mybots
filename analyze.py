06134042757a75c8f1227860d27d3b11d93b9327
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-16 11:42:47 -0600

    load and plot front leg sensor values into analyze.py

--- analyze.py
+++ analyze.py
@@ -2,6 +2,11 @@
 import matplotlib.pyplot
 import numpy
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy',mmap_mode ='r', allow_pickle= False, fix_imports = False) #load data/backLegSensorValues.npy (for reading only = 'mmap_mode = r' into the vector backLegSensorValues.
+frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy',mmap_mode ='r', allow_pickle= False, fix_imports = False) #load data/frontLegSensorValues.npy (for reading only = 'mmap_mode = r' into the vector frontLegSensorValues.
 print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
+print("frontLegSensorValues = ",frontLegSensorValues) #printing array of frontLegSensorValues
+
 matplotlib.pyplot.plot(backLegSensorValues) #plot the values of backLegSensorValues array
+matplotlib.pyplot.plot(frontLegSensorValues) #plot the values of frontLegSensorValues array
+
 matplotlib.pyplot.show() #show the above plot
