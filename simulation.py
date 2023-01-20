16c0463e99785560159885f6675edfba3bad27ba
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-20 15:47:56 -0600

    SIMULATION contains a WORLD and a ROBOT

--- simulation.py
+++ simulation.py
@@ -1,5 +1,8 @@
 #simulation.py
-class SIMULATION:           #define a class, SIMULATION
-    def __init__(self):     #defines the init constructor (AKA method) for the SIMULATION class
-        pass
-        
+from world import WORLD
+from robot import ROBOT
+
+class SIMULATION:               #define a class, SIMULATION
+    def __init__(self):         #defines the init constructor (AKA method) for the SIMULATION class
+        self.world = WORLD()    #creates a new SIMULATION attribute, and that attribute will hold an instance of the WORLD class
+        self.robot = ROBOT()    #creates a new SIMULATION attribute, and that attribute will hold an instance of the ROBOT class
