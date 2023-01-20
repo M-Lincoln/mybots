f2651991bf28b59b178b00af0c3b3936eaf1e7ce
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-20 17:02:32 -0600

    move for loop from simulate.py to simulation.py

--- simulate.py
+++ simulate.py
@@ -9,9 +9,9 @@ from simulation import SIMULATION #Include the SIMULATION class
 
 #import random #need this package for returning random numbers
 #import matplotlib.pyplot 
-#import constants as c
-simulation = SIMULATION() #Create an object (instance) of the SIMULATION class called 'simulation'
 
+simulation = SIMULATION() #Create an object (instance) of the SIMULATION class called 'simulation'
+simulation.Run()
 
 
 
@@ -35,32 +35,6 @@ simulation = SIMULATION() #Create an object (instance) of the SIMULATION class c
 ##matplotlib.pyplot.show()
 
 
-#for i in range(c.iterationLength): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
-#    p.stepSimulation()
-#    ##add back leg sensor and track values
-#    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg") #add a touch sensor to the back leg
-#    print("backLegTouch sensor value = %d" %backLegSensorValues[i]) #print the sensor value of backLegTouch
-#    ##add front leg sensor and track values
-#    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("frontleg") #add a touch sensor to the front leg
-#    print("frontLegTouch sensor value = %d" %frontLegSensorValues[i]) #print the sensor value of frontLegTouch
-    
-    
-
-#    ##simulate a motor for joint 'torso_backleg'
-#    pyrosim.Set_Motor_For_Joint(
-#        bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
-#        jointName = b'torso_backleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting back leg and torso
-#        controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
-#        targetPosition = targetAngles_backleg[i], # desired position (desired angle) between the 2 links connected by the joint
-#        maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
-
-#    ##simulate a motor for joint 'torso_frontleg'
-#    pyrosim.Set_Motor_For_Joint(
-#        bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
-#        jointName = b'torso_frontleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting front leg and torso
-#        controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
-#        targetPosition = targetAngles_frontleg[i], # desired position (desired angle) between the 2 links connected by the joint
-#        maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
 
 #time.sleep(c.sleepTime) #time.sleep(0.005) is nice viewing time, not too slow
 #    print(i) 
--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 #simulation.py
 import pyrosim.pyrosim as pyrosim #import pyrosim
 import pybullet as p
+import constants as c
 import pybullet_data
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,3 +15,28 @@ class SIMULATION:               #define a class, SIMULATION
         self.robot = ROBOT()    #creates a new SIMULATION attribute, and that attribute will hold an instance of the ROBOT class
         self.robotID = p.loadURDF("body.urdf") #add a torso to the environment 
         pyrosim.Prepare_To_Simulate(self.robotID) #pyrosim needs to set up for simulating sensors. robotID contains an integer, indicating which robot you want prepared for simulation
+
+    def Run(self):
+        for i in range(c.iterationLength): #for loop going from 0-999, end with colon and make sure next line is indented. don't need an "end" statement because it will end once no longer indented
+            print("iteration number ", i)
+        #p.stepSimulation()
+        ##add back leg sensor and track values
+        #backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg") #add a touch sensor to the back leg
+        #print("backLegTouch sensor value = %d" %backLegSensorValues[i]) #print the sensor value of backLegTouch
+        ##add front leg sensor and track values
+        #frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("frontleg") #add a touch sensor to the front leg
+        #print("frontLegTouch sensor value = %d" %frontLegSensorValues[i]) #print the sensor value of frontLegTouch
+        ##simulate a motor for joint 'torso_backleg'
+        #pyrosim.Set_Motor_For_Joint(
+        #bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
+        #jointName = b'torso_backleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting back leg and torso
+        #controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
+        #targetPosition = targetAngles_backleg[i], # desired position (desired angle) between the 2 links connected by the joint
+        #maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
+        ##simulate a motor for joint 'torso_frontleg'
+        #pyrosim.Set_Motor_For_Joint(
+        #bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
+        #jointName = b'torso_frontleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting front leg and torso
+        #controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
+        #targetPosition = targetAngles_frontleg[i], # desired position (desired angle) between the 2 links connected by the joint
+        #maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
