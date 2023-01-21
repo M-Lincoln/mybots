eb11abbe5789d96dc98bac1b4466862e8a6cc57d
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-21 17:40:19 -0600

    try to integrate the motor controls

--- motor.py
+++ motor.py
@@ -19,9 +19,10 @@ class MOTOR:
     
     def Set_Value(self):
         ##simulate a motor for joint 'torso_frontleg'
-        #pyrosim.Set_Motor_For_Joint(
-        #bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
-        #jointName = b'torso_frontleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting front leg and torso
-        #controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
-        #targetPosition = targetAngles_frontleg[i], # desired position (desired angle) between the 2 links connected by the joint
-        #maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
+        ###DUNNO WHAT TO DO HERE!!! self.robot = ROBOT()
+        pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
+        jointName = b'torso_frontleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting front leg and torso
+        controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
+        targetPosition = targetAngles_frontleg[i], # desired position (desired angle) between the 2 links connected by the joint
+        maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
--- simulate.py
+++ simulate.py
@@ -22,20 +22,6 @@ simulation.Run()
 #print("targetAngles_backleg = ",targetAngles_backleg)
 #print("targetAngles_frontleg = ",targetAngles_frontleg) 
 
-##simulate a motor for joint 'torso_backleg'
-        #pyrosim.Set_Motor_For_Joint(
-        #bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
-        #jointName = b'torso_backleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting back leg and torso
-        #controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
-        #targetPosition = targetAngles_backleg[i], # desired position (desired angle) between the 2 links connected by the joint
-        #maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
-        ##simulate a motor for joint 'torso_frontleg'
-        #pyrosim.Set_Motor_For_Joint(
-        #bodyIndex = robotID, #tells simulator what robot the motor should be attached to (which is called 'robot' in this case)
-        #jointName = b'torso_frontleg', #tells the simulator what joint the motor should be attached to. in this case, the joint connecting front leg and torso
-        #controlMode = p.POSITION_CONTROL, #defines the type of control we are using (either position control or velocity control)
-        #targetPosition = targetAngles_frontleg[i], # desired position (desired angle) between the 2 links connected by the joint
-        #maxForce = c.defineMaxForce) #cap the total torque used by the motor [500 Nm]
 
 ##numpy.save(os.path.join('data','targetAngles_backleg'),targetAngles_backleg, allow_pickle=False, fix_imports=False) #save an array to a binary file in Numpy, .npy format, in a different folder called "data"
 ##numpy.save(os.path.join('data','targetAngles_frontleg'),targetAngles_frontleg, allow_pickle=False, fix_imports=False) #save an array to a binary file in Numpy, .npy format, in a different folder called "data"
