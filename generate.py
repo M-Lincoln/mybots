c656ccb5560013d2b061b730b75cbb9b3e087652
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-25 17:19:13 -0600

    trial of modifying generate.py

--- generate.py
+++ generate.py
@@ -11,6 +11,19 @@ z1=height/2 #makes it so that the initial position of the box is at ground-level
 xworld=5
 yworld=5
 zworld=height/2
-from Functions import Create_World, Create_Robot #I, Noah J Katz, called a function from "Functions", specifically the function from "Functions" was called Create_World.
+from Functions import Create_World #I, Noah J Katz, called a function from "Functions", specifically the function from "Functions" was called Create_World.
+def Generate_Body(x1,y1,z1,width,length,height):
+	pyrosim.Start_URDF("body.urdf")
+
+	#create a robot with an abdomen and 2 legs:
+	pyrosim.Send_Cube(name="torso", pos=[0,0,1.5] , size=[width,length,height]) #stores a box with initial position x, y, z and length, width, and height, in body.urdf
+	#create backleg
+	pyrosim.Send_Joint( name = "torso_backleg" , parent= "torso" , child = "backleg" , type = "revolute", position = [-0.5,0,1]) #Joint
+	pyrosim.Send_Cube(name="backleg", pos=[-0.5,0,-0.5] , size=[width,length,height]) #stores a box with initial position x, y, z and length, width, and height, in body.urdf
+	#create frontleg
+	pyrosim.Send_Joint( name = "torso_frontleg" , parent= "torso" , child = "frontleg" , type = "revolute", position = [0.5,0,1]) #Joint
+	pyrosim.Send_Cube(name="frontleg", pos=[0.5,0,-0.5] , size=[width,length,height]) #stores a box with initial position x, y, z and length, width, and height, in body.urdf
+
+
 Create_World(xworld,yworld,zworld,width,length,height) #you must put 6 variables into this function from "Functions" otherwise it blows up.
-Create_Robot(x1,y1,z1,width,length,height)
+Generate_Body(x1,y1,z1,width,length,height)
