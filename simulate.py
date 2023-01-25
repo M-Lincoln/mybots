dd106fb7ae3d58a8e92cfd6713f5f3c94a567831
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-25 11:41:23 -0600

    clean up simulate.py

--- simulate.py
+++ simulate.py
@@ -1,40 +1,4 @@
-#trial of simulate.py
 from simulation import SIMULATION #Include the SIMULATION class 
-
-#from cmath import pi
-
-
-#import time
-#import numpy
-
-#import random #need this package for returning random numbers
-#import matplotlib.pyplot 
 
 simulation = SIMULATION() #Create an object (instance) of the SIMULATION class called 'simulation'
 simulation.Run()
-
-
-
-
-
-###closed loop control 
-
-#print("targetAngles_backleg = ",targetAngles_backleg)
-#print("targetAngles_frontleg = ",targetAngles_frontleg) 
-
-
-
-##matplotlib.pyplot.plot(x, targetAngles)
-##matplotlib.pyplot.xlabel('Angle [rad]')
-##matplotlib.pyplot.ylabel('sin(x)')
-##matplotlib.pyplot.axis('tight')
-##matplotlib.pyplot.show()
-
-
-
-#time.sleep(c.sleepTime) #time.sleep(0.005) is nice viewing time, not too slow
-#    print(i) 
-#    #to move the camera, control+click and drag with a mouse, or 2-fingered swipe on trackpad for zooming in/out
-
-#print("backLegSensorValues = ",backLegSensorValues) #printing array of backLegSensorValues
-#print("frontLegSensorValues = ",frontLegSensorValues) #printing array of frontLegSensorValues
