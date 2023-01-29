e570560eee4e0ba3a1742c0ce23d34d7f4ca5a7a
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-29 14:42:42 -0600

    Thru L.18

--- solution.py
+++ solution.py
@@ -4,7 +4,5 @@ import numpy
 class SOLUTION:
     def __init__(self):
         self.weights = numpy.random.rand(3,2)     #create a 3x2 matrix filled with random numbers b/w 0 and 1. If you want the weight of the synapse that connects the third sensor neuron to the second motor neuron, for example, you would "walk down" to the third row, and then "walk right" to the second column.
-        print("weights before operation = ", self.weights)
-        self.weights = 2*self.weights -1
-        print("weights after operation = ", self.weights)
-        exit()
+        self.weights = 2*self.weights -1          #shift range of random numbers to [-1,1]
+ 
