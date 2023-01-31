f6bb20ac375765333144f7df3b07d31fa0903cbc
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-01-31 17:44:10 -0600

    Thru M.14 Create parallel hill climber

---
+++ parallelHillClimber.py
@@ -0,0 +1,40 @@
+#hillclimber.py
+from solution import SOLUTION
+import constants as c
+import copy
+import simulation
+
+class PARALLEL_HILL_CLIMBER:
+    def __init__(self):
+        self.parent = SOLUTION()        #create an instance of SOLUTION
+
+    def Evolve(self):
+        #self.parent.Evaluate("GUI")
+        #for currentGeneration in range(c.numberOfGenerations): 
+        ##for currentGeneration in range(3):  #view behavior of the FIRST randomly generated solution
+        #    self.Evolve_For_One_Generation()
+        pass
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)     #self.child will receive a copy of self.parent's weights, as well as its fitness.
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if self.child.fitness<self.parent.fitness:      #replace the parent with its child if the parent does worse
+            self.parent = self.child
+
+    def Print(self):
+        print("parent's fitness = ", self.parent.fitness, "; child's fitness = ", self.child.fitness)       #print the fitness of parent and child
+        
+    def Show_Best(self):
+        #self.parent.Evaluate("GUI")     #try to evaluate the parent with graphics turned on
+        pass
--- search.py
+++ search.py
@@ -1,12 +1,12 @@
 #search.py alternately generates and simulates robots with different sets of synaptic weights
 import os
-from hillclimber import HILL_CLIMBER
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
 #for index in range(5):
 #    os.system("python .\generate.py")
 #    os.system("python .\simulate.py")
 #    index=index+1
 
-hc = HILL_CLIMBER()
-hc.Evolve()     #call Evolve() 
-hc.Show_Best()
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()     #call Evolve() 
+phc.Show_Best()
