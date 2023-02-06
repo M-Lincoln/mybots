3c7ef8a00afa92e48f946b7e14095cefe595ceec
M-Lincoln <madelinelincoln2023@u.northwestern.edu>
2023-02-06 14:36:39 -0600

    Thru O.2

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ import time
 class SOLUTION:
 	def __init__(self,nextAvailableID):
 		self.myID = nextAvailableID
-		self.weights = numpy.random.rand(c.numSensorNeurons,c.numMotorNeurons)     #create a 3x2 matrix filled with random numbers b/w 0 and 1. If you want the weight of the synapse that connects the third sensor neuron to the second motor neuron, for example, you would "walk down" to the third row, and then "walk right" to the second column.
+		self.weights = numpy.random.rand(c.numSensorNeurons,c.numMotorNeurons)     #create a matrix filled with random numbers b/w 0 and 1. If you want the weight of the synapse that connects the third sensor neuron to the second motor neuron, for example, you would "walk down" to the third row, and then "walk right" to the second column.
 		self.weights = 2*self.weights-1          #shift range of random numbers to [-1,1]
 		
 	def Start_Simulation(self,directOrGUI):
